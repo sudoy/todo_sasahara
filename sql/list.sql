e99d2bf6b8a74cbfaecadf9fb9921b555f7945c4
hayate-sasahara <hayate-sasahara@tsd-201511-0001>
2018-05-29 13:08:31 +0900

    02 NOT NULL追加 クォーテション訂正

--- sql/list.sql
+++ sql/list.sql
@@ -1,16 +1,16 @@
 CREATE TABLE list(
 	id INT PRIMARY KEY AUTO_INCREMENT,
-	title VARCHAR(100),
-	detail VARCHAR(100),
-	imp VARCHAR(3),
+	title VARCHAR(100) NOT NULL,
+	detail VARCHAR(100) NOT NULL,
+	imp VARCHAR(3) NOT NULL,
 	limit_date DATE
 );
 
 INSERT INTO list(title, detail, imp, limit_date)
-VALUES("テスト", "SQLのテスト", "★★★", "2015-06-20");
+VALUES('テスト', 'SQLのテスト', '★★★', '2015-06-20');
 INSERT INTO list(title, detail, imp, limit_date)
-VALUES("テスト2", "Javaのテスト", "★", "2015-06-21");
+VALUES('テスト2', 'Javaのテスト', '★', '2015-06-21');
 INSERT INTO list(title, detail, imp, limit_date)
-VALUES("テスト3", "HTMLのテスト", "★★", "2015-06-24");
+VALUES('テスト3', 'HTMLのテスト', '★★', '2015-06-24');
 INSERT INTO list(title, detail, imp)
-VALUES("テスト4", "なにかのテスト", "★");
+VALUES('テスト4', 'なにかのテスト', '★');
